6fc7b8c8b9e68c8cc0ba7c5bf7409b36a65fffba
Seán Quill <osmosae@gmail.com>
2023-03-13 19:12:45 +0000

    removing unnecessary comments

--- server/models/database.js
+++ server/models/database.js
@@ -8,25 +8,5 @@ db.once("open", function () {
     console.log("Connected to MongoDB")
 })
 
-// const mongoose = require("mongoose")
-
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.DB_STRING, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             useFindAndModify: false,
-//             useCreateIndex: true,
-//         })
-
-//         console.log(`MongoDB Connected: ${conn.connection.host}`)
-//     } catch (err) {
-//         console.error(err)
-//         process.exit(1)
-//     }
-// }
-
 //  Models
 require("./Category")
-
-// module.exports = connectDB
